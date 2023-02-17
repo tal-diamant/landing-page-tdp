e7050e90d6282c8c296a0186a07a13117e0f158e
tal-diamant <erkeliwood@gmail.com>
2023-02-17 17:46:40 +0700

    feat: finish home hero section

---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,21 @@
+import Hero from "@/modules/Home/containers/Hero/Hero";
+
+export default function Home() {
+  return (
+    <>
+      {/* header */}
+
+      <Hero /> {/* <-- */}
+      {/* Customers */}
+      {/* features */}
+      {/* Process */}
+      {/* info */}
+      {/* testimonials */}
+      {/* fit */}
+      {/* faq */}
+      {/* cta */}
+
+      {/* footer */}
+    </>
+  );
+}
